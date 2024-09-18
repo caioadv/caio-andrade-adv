1473ce2683c2eb16ace23cf70a8a50e84b51726e
caioadv <caiovianaandrade@outlook.com>
2024-09-18 17:18:48 -0300

    Update script.js

--- Advogado Caio Andrade/script.js
+++ Advogado Caio Andrade/script.js
@@ -1,9 +1,20 @@
-window.addEventListener('scroll', function() {
-    const header = document.querySelector('.site-header-wrapper');
-    const logoWrapper = document.querySelector('.logo-wrapper');
-    if (window.scrollY > 100) {
-      header.classList.add('nav-shrink');
-    } else {
-      header.classList.remove('nav-shrink');
-    }
+document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      const targetId = this.getAttribute('href');
+      const targetSection = document.querySelector(targetId);
+      const targetTitle = targetSection.querySelector('.section-title');
+
+      window.scrollTo({
+          top: targetSection.offsetTop - 50,
+          behavior: 'smooth'
+      });
+
+      targetTitle.classList.add('highlight');
+
+      setTimeout(() => {
+          targetTitle.classList.remove('highlight');
+      }, 2000);
   });
+});
